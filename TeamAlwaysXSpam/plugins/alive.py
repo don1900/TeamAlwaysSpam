99f543b718613e1f6ac37e16110e029b48367f6e
don1900 <80756375+don1900@users.noreply.github.com>
2022-01-23 11:10:12 +0545

    Create alive.py

---
+++ TeamAlwaysXSpam/plugins/alive.py
@@ -0,0 +1,36 @@
+from .. import Don, SUDO_USERS, alwaysversion
+from .. import ALIVE_PIC
+from telethon import events, version, Button
+from telethon.tl.custom import button
+from time import time
+from datetime import datetime
+
+DON_PIC = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/4cc84cabf3ee789d64ce0.jpg"
+  
+
+          
+always = "**Tᴇᴀᴍ Aʟᴡᴀʏꜱ Sᴘᴀᴍ Bᴏᴛ Iꜱ Wᴏʀᴋɪɴɢ Pᴇʀꜰᴇᴄᴛʟʏ♠️♠️**\n\n"
+
+always += f"🔥**TᴇᴀᴍAʟᴡᴀʏꜱ Sᴘᴀᴍ Sʏꜱᴛᴇᴍ**🔥\n\n"
+
+always += f" **•⭕️• Pʏᴛʜᴏɴ Vᴇʀsɪᴏɴ** : `3.9.6`\n"
+
+always += f" **•⭕️• Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ** : `{version.__version__}`\n"
+
+always += f" **•⭕️• Bot Vᴇʀsɪᴏɴ**  : `{alwaysversion}`\n"
+
+always += f" **•⭕️• Cʀᴇᴀᴛᴏʀ**  : [Dᴏɴ](t.me/Always_Don)\n\n"
+         
+                                    
+@Don.on(events.NewMessage(pattern=".robot"))
+async def alive(event):
+    if event.sender_id in SUDO_USERS:
+     await Don.send_file(event.chat_id,
+                                  DON_PIC,
+                                  caption=always,
+                                  buttons=[
+        [
+        Button.url("Cᴏᴍᴍᴜɴɪᴛʏ", "https://t.me/Team_Always")
+        ]
+        ]
+        
